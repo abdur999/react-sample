eb75c80eb92dd49c7dd9f05a99d0f496b4c05b68
abdur999 <abdursrahims@icloud.com>
2024-07-17 23:29:40 +0530

    React app created, dlete system crated react app and create custom element

---
+++ samplereact/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const tasks = ["Take out the trash","Shovel out the trash","Grape Vine"]
+
+//Static content creation
+
+// const element = React.createElement('ul', null,
+//   React.createElement('li',null,"Take out the trash"),
+//   React.createElement('li',null,"Shovel out the trash"),
+//   React.createElement('li',null,"Grape Vine")
+// )
+// ReactDOM.render(element, document.getElementById('root'))
+
+
+const element = React.createElement('ol', null, 
+  tasks.map((task,index) => React.createElement("li",{ key: index }, task))
+)
+ReactDOM.render(element, document.getElementById('root'))
